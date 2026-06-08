00b193b4fedb3296159f68861222a293ab740982
Guilherme Colatino <AleatorioWorld@gmail.com>
2026-06-08 17:23:33 -0300

    Criação da classe Main e correção de logica

--- src/main/java/domain/strategy/DivisaoPorPorcentagem.java
+++ src/main/java/domain/strategy/DivisaoPorPorcentagem.java
@@ -16,23 +16,28 @@ public class DivisaoPorPorcentagem implements Divisao {
 
     @Override
     public Map<String, Dinheiro> calcular(Dinheiro valorTotal, List<String> participantes) {
-
         Map<String, Dinheiro> resultado = new HashMap<>();
+        int totalCentavosDistribuidos = 0;
 
         for (String participante : participantes) {
             Double porcentagem = porcentagens.get(participante);
 
             if (porcentagem == null) {
-                throw new IllegalArgumentException(
-                        "Porcentagem não definida para " + participante
-                );
+                throw new IllegalArgumentException("Porcentagem não definida para " + participante);
             }
 
-            int valor = (int) Math.round(
-                    valorTotal.getCentavos() * (porcentagem / 100.0)
-            );
-
+            int valor = (int) Math.round(valorTotal.getCentavos() * (porcentagem / 100.0));
             resultado.put(participante, new Dinheiro(valor));
+            totalCentavosDistribuidos += valor;
+        }
+
+        int diferenca = valorTotal.getCentavos() - totalCentavosDistribuidos;
+        if (diferenca != 0 && !participantes.isEmpty()) {
+            String primeiroId = participantes.get(0);
+            Dinheiro valorAtual = resultado.get(primeiroId);
+
+            int novoValorCentavos = valorAtual.getCentavos() + diferenca;
+            resultado.put(primeiroId, new Dinheiro(novoValorCentavos));
         }
 
         return resultado;
